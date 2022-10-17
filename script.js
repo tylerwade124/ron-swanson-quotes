e35291bd3014570984e2f9da7a329e45bbc6b8d8
tyler <tylerwade124@gmail.com>
2022-10-17 10:33:15 -0400

    data being pulled properly, everything working as intended

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log(`working`)
+// console.log(`working`)
+
+//GLOBAL variables
+
+let searchButton = document.querySelector(`#searchButton`)
+
+//FUNCTIONS/FETCH URL
+
+async function getData (event) {
+    event.preventDefault()
+    let textInput = document.querySelector(`#inputbar`).value.toLowerCase()
+
+    fetch(`http://ron-swanson-quotes.herokuapp.com/v2/quotes/search/${textInput}`)
+        .then(res => {
+            return res.json()
+        })
+        .then (res => {
+            console.log(`success`, res)
+            let quote = document.querySelector(`#quoteText`)
+            quote.innerText = res
+        })
+        .catch (err => {
+            console.log("error!", err)
+        })
+
+
+
+}
+
+
+//EVENT LISTENERS
+
+searchButton.addEventListener(`click`, getData)
